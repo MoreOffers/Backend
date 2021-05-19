2073d7be08c6c44019a15c118f872a7268b71ba1
TinaLand <huihuijiang0619@gmail.com>
2021-05-18 20:13:40 -0400

    Init commit

---
+++ src/main/java/AIDeliver/com/example/AIDeliver/AiDeliverApplication.java
@@ -0,0 +1,13 @@
+package AIDeliver.com.example.AIDeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDeliverApplication.class, args);
+	}
+
+}
