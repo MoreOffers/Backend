23a8b3550f1717254e78100b54beda192d77bffd
liana3420 <67452380+liana3420@users.noreply.github.com>
2021-05-20 21:47:25 -0700

    Init commit

--- src/main/java/AIDeliver/com/example/AIDeliver/repository/UserRepository.java
+++ src/main/java/AIDeliver/com/example/AIDeliver/repository/UserRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    //use Optional to prevent npe
     Optional<User> findUserByEmail(String email);
     User findUserByName(String name);
 }
