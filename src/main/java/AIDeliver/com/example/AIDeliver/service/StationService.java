ea39149861a2ae07e8034e6c65b09f1293706211
TinaLand <huihuijiang0619@gmail.com>
2021-05-24 09:20:32 -0400

    Added service annotation

--- src/main/java/AIDeliver/com/example/AIDeliver/service/StationService.java
+++ src/main/java/AIDeliver/com/example/AIDeliver/service/StationService.java
@@ -1,10 +1,11 @@
 package AIDeliver.com.example.AIDeliver.service;
 
 import AIDeliver.com.example.AIDeliver.enity.Station;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
+@Service
 public interface StationService {
     List<Station> getAllStations();
 }
