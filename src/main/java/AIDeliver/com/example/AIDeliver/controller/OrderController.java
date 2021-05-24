4973867c81a2b46a38260182ce084150d1e1a9e0
TinaLand <huihuijiang0619@gmail.com>
2021-05-24 11:06:30 -0400

    Update order url path

--- src/main/java/AIDeliver/com/example/AIDeliver/controller/OrderController.java
+++ src/main/java/AIDeliver/com/example/AIDeliver/controller/OrderController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "salesOrder")
+@RequestMapping(path = "order")
 
 public class OrderController {
 
