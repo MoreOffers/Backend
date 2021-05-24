82f4c0e045788cfc8a66c22a3bfb651095f8b7cc
bebe7939 <82177635+bebe7939@users.noreply.github.com>
2021-05-24 14:35:32 -0400

    Update ZipCode.java

--- src/main/java/AIDeliver/com/example/AIDeliver/service/Impl/ZipCode.java
+++ src/main/java/AIDeliver/com/example/AIDeliver/service/Impl/ZipCode.java
@@ -11,52 +11,52 @@ import java.util.Map;
 public class ZipCode {
 
 
-//    public Map<String, Integer> calTable;
-//    public ZipCode(){
-//        calTable = new HashMap<>() {
-//            //key: fake sender zipcode-receiver zip code; value: distance
-//            {put("10000-20000", 10);}
-//            {put("10000-30000", 20);}
-//            {put("10000-40000", 30);}
-//            {put("10000-50000", 40);}
-//            {put("10000-60000", 50);}
-//        };
-//    }
+   public Map<String, Integer> calTable;
+   public ZipCode(){
+       calTable = new HashMap<>() {
+           //key: fake sender zipcode-receiver zip code; value: distance
+           {put("10000-20000", 10);}
+           {put("10000-30000", 20);}
+           {put("10000-40000", 30);}
+           {put("10000-50000", 40);}
+           {put("10000-60000", 50);}
+       };
+   }
 
 
 
 
-        HashMap<Coordinate, Integer> calTable = new HashMap<>() {
-            {
-                put(new Coordinate("10000", "20000"), 10);
-            }
+//         HashMap<Coordinate, Integer> calTable = new HashMap<>() {
+//             {
+//                 put(new Coordinate("10000", "20000"), 10);
+//             }
 
-            ;
+//             ;
 
-            {
-                put(new Coordinate("10000", "30000"), 20);
-            }
+//             {
+//                 put(new Coordinate("10000", "30000"), 20);
+//             }
 
-            ;
+//             ;
 
-            {
-                put(new Coordinate("10000", "40000"), 30);
-            }
+//             {
+//                 put(new Coordinate("10000", "40000"), 30);
+//             }
 
-            ;
+//             ;
 
-            {
-                put(new Coordinate("10000", "50000"), 40);
-            }
+//             {
+//                 put(new Coordinate("10000", "50000"), 40);
+//             }
 
-            ;
+//             ;
 
-            {
-                put(new Coordinate("10000", "60000"), 50);
-            }
+//             {
+//                 put(new Coordinate("10000", "60000"), 50);
+//             }
 
 
 
-        };
+//         };
     }
 
